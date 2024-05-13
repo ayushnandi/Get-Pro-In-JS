45739752ec96c4a71a804cc0254fda79e6081fa8
Ayush Nandi <110190468+ayushnandi@users.noreply.github.com>
2024-05-13 20:44:19 +0530

    sdsds

--- Node JS/TestNode2/index.js
+++ Node JS/TestNode2/index.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const port = 3000
+const express = require("express");
+const port = 3000;
 const app = express();
-const  zod  = require("zod");
+const zod = require("zod");
 
 // ZOD is used when need to validate(check weather provided
 // input by user is correct or not string for string and
 // integer for integer) the input provided by user
 
-function validateInput (arr){
-    const schema = zod.arr(zod.number());
-    const response = schema.safeParse(arr);
-    console.log(response);
+function validateInput(arr) {
+  const schema = zod.array(zod.number());
+  const response = schema.safeParse(arr);
+  console.log(response);
 }
-validateInput([1,2,3,4]);
+validateInput([1, 2, 3, 4]);
 
-app.listen(port, ()=> {
-    console.log("server is running");
-})
+app.listen(port, () => {
+  console.log("server is running");
+});
